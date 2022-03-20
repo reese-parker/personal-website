c78baa48a0b6c43ab823feabfc4f7867ad7953f5
Reese Parker <parker.rhys@outlook.com>
2022-03-20 13:01:25 -0400

    Created projects data object for project cards

---
+++ src/projectsData.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    name: "Todo App",
+    description: "A to-do app for easy task management.",
+    technologies: ["React", "Material UI", "Web Storage API"],
+    projectLink: "https://reese-parker.github.io/todo-app/",
+    githubLink: "https://github.com/reese-parker/todo-app",
+  },
+  {
+    name: "Personal Website",
+    description: "A personal website showcasing projects and contact links.",
+    technologies: ["React", "Firebase", "Material UI"],
+    projectLink: "https://reeseparker.com",
+    githubLink: "https://github.com/reese-parker/personal-website",
+  },
+  {
+    name: "Joke Generator",
+    description: "A joke generator for randomized dad jokes.",
+    technologies: ["React", "Axios", "CSS Modules"],
+    projectLink: "https://reese-parker.github.io/joke-generator/",
+    githubLink: "https://github.com/reese-parker/joke-generator",
+  },
+];
