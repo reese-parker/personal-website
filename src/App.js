a5cf2692f283f8eb43c0836bcd33ecd44a5e3918
Reese Parker <parker.rhys@outlook.com>
2022-03-20 13:04:30 -0400

    Added components to render

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+import Drawer from "@mui/material/Drawer";
+
+import styles from "./styles/AppStyles";
+import useToggleState from "./hooks/useToggleState";
+import AppLoadingBar from "./AppLoadingBar";
+import Navbar from "./Navbar";
+import AboutSection from "./AboutSection";
+import ProjectsSection from "./ProjectsSection";
+import ContactSection from "./ContactSection";
+import Menu from "./Menu";
 
 function App() {
+  const [isDrawerOpen, toggleDrawer] = useToggleState(false);
+  const [isAppLoading, toggleAppLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      toggleAppLoading(false);
+    }, 1500);
+  }, []);
+
   return (
-    <div className="App">
-     m8
-    </div>
+    <>
+        <Navbar handleOpenDrawer={toggleDrawer} />
+        {isAppLoading ? (
+          <AppLoadingBar isAppLoading={isAppLoading} />
+        ) : (
+          <>
+            <AboutSection isLoading={isAppLoading} />
+            <ProjectsSection />
+            <ContactSection />
+          </>
+        )}
+    
+
+      <Drawer
+        anchor={"left"}
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+        PaperProps={{
+          sx: styles.DrawerPaperProps,
+        }}
+        elevation={1}
+      >
+        <Menu handleCloseDrawer={toggleDrawer} />
+      </Drawer>
+    </>
   );
 }
 
