6eb807c94071e8475063b97ea3c1fb5a4a68c99d
Reese Parker <parker.rhys@outlook.com>
2022-04-24 16:13:25 -0400

    Updated projects data and added tests

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,5 +1,12 @@
 export const projects = [
   {
+    name: "Hockey Jersey Emporium",
+    description: "An e-commerce store integrated with Stripe for secure payments.",
+    technologies: ["React", "Commerce.js", "Stripe.js", "React Hook Form"],
+    projectLink: "https://reese-parker.github.io/ecommerce-app/",
+    githubLink: "https://github.com/reese-parker/ecommerce-app",
+  },
+  {
     name: "Social Media Clone",
     description: "A social media app where users can sign up for an account and post messages.",
     technologies: ["React", "Firebase Auth/Firestore", "React-Router"],
@@ -12,6 +19,13 @@ export const projects = [
     technologies: ["React", "Material UI", "Web Storage API"],
     projectLink: "https://reese-parker.github.io/todo-app/",
     githubLink: "https://github.com/reese-parker/todo-app",
+  },
+  {
+    name: "Shopify Homepage Clone",
+    description: "A responsive replica of Shopify's homepage",
+    technologies: ["React", "CSS",],
+    projectLink: "https://reese-parker.github.io/shopify-homepage-clone/",
+    githubLink: "https://github.com/reese-parker/shopify-homepage-clone",
   },
   {
     name: "Personal Website",
