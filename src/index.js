c2a552f33b4b67ad7c170dc87c0070506794418e
Reese Parker <parker.rhys@outlook.com>
2022-04-26 15:51:17 -0400

    Updated favicon

--- src/index.js
+++ src/index.js
@@ -3,26 +3,23 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ThemeProvider } from "@mui/material/styles";
-import { darkTheme } from "./styles/themes";
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from "firebase/analytics";
+import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
 
+import { darkTheme } from "./styles/themes";
+import { initializeApp } from "firebase/app";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <StyledEngineProvider injectFirst>
     <ThemeProvider theme={darkTheme}>
-      
-        <App />
-     
+      <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </StyledEngineProvider>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
 
 reportWebVitals();
-
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyBWwsbVzwf2xVg_4c9G1m6Qd057lGXQsh0",
@@ -31,8 +28,9 @@ const firebaseConfig = {
   storageBucket: "personal-website-676d8.appspot.com",
   messagingSenderId: "931419566873",
   appId: "1:931419566873:web:933afe3fcba4c188218b88",
-  measurementId: "G-0GVWTLXSP9"
+  measurementId: "G-0GVWTLXSP9",
 };
 
+
+// eslint-disable-next-line no-unused-vars
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics();
