ce7a1466a6037e39d14adeeaad5af142b62eed45
Reese Parker <parker.rhys@outlook.com>
2022-04-26 15:50:47 -0400

    Created separate folder for data

---
+++ src/data/contactData.js
@@ -0,0 +1,16 @@
+const contactData = [
+  {
+    contactMethod: "Email",
+    link: "mailto:parker.rhys@outlook.com",
+  },
+  {
+    contactMethod: "LinkedIn",
+    link: "https://www.linkedin.com/in/reese-parker/",
+  },
+  {
+    contactMethod: "GitHub",
+    link: "https://github.com/reese-parker",
+  },
+];
+
+export default contactData
